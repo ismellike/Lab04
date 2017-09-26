a0b2f2ddd9c92aeb8ceba05ade62013232ee87ee
Gabriel Lopez <g137l275@ku.edu>
2017-09-26 15:37:58 -0500

    Finished Exercise 1

--- Lab04/Table.php
+++ Lab04/Table.php
@@ -1,3 +1,20 @@
 <?php
-   echo "<p>Hello, World!</p>";
+	echo "<table>";
+   for($y = 0; $y <= 100; $y++) 
+   {
+	   echo "<tr>";
+	   for($x = 0; $x <= 100; $x++)
+	   {
+		   if($x == 0 && $y == 0)
+			   echo "<td> </td>";
+		   else if($y == 0)
+			   echo "<td>".$x."</td>";
+		   else if($x == 0)
+			   echo "<td>".$y."</td>";
+		   else
+			echo "<td>".$y*$x."</td>";
+	   }
+	   echo "</tr>";
+   }
+   echo "</table>";
 ?>
